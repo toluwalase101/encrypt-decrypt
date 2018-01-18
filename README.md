13a83c399a84dbf65eea7c300550757f3c07d578
Matt Mcnamee <matt@mcnamee.co>
2018-01-18 15:48:37 +1000

    Init

---
+++ README.md
@@ -0,0 +1,15 @@
+# Encrypt and Decrypt Messages
+
+Input a 'password' and use it to encrypt or decrypt text.
+
+## Install and Usage
+
+```bash
+# Install dependencies
+npm i
+
+# Build and run
+npm start
+
+# Open your browser at http://127.0.0.1:8080
+```
---
+++ js/main.js
@@ -0,0 +1,21 @@
+window.onload = function () {
+  document.getElementById("encryptForm").addEventListener("submit", function(e) {
+    e.preventDefault();
+
+    // Values
+    var key = document.getElementById('key').value;
+    var toEncrypt = document.getElementById('encrypt').value;
+    var toDecrypt = document.getElementById('decrypt').value;
+
+    // Create an encryptor
+    var encryptor = require('simple-encryptor')(key);
+
+    // To Encrypt
+    var encrypted = encryptor.encrypt(toEncrypt);
+    document.getElementById("encrypted").innerHTML=encrypted;
+
+    // To Decrypt
+    var decrypted = encryptor.decrypt(toDecrypt);
+    document.getElementById("decrypted").innerHTML=decrypted;
+  });
+}
