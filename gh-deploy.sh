cee2607aa10d001b97677f79cb6f7077a992113a
Matt Mcnamee <matt@mcnamee.co>
2018-01-24 13:01:17 +1000

    Update gh-deploy

--- gh-deploy.sh
+++ gh-deploy.sh
@@ -3,5 +3,5 @@ rm -rf build;
 npm run build;
 mkdir build/build && mv build/bundle.js build/build/bundle.js && mv build/bundle.js.map build/build/bundle.js.map;
 cp index.html build/index.html;
-git add build/* && git commit -m 'JS bundled for GitHub Pages deploy';
+git add build/* -f && git commit -m 'JS bundled for GitHub Pages deploy';
 git subtree push --prefix build origin gh-pages;
