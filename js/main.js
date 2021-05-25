a850a5c8a839b90e039589a211a7462fdfac6b3d
Matt McNamee <matt@mcnamee.co>
2021-05-26 08:45:33 +1000

    Add an error message

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 window.onload = function () {
-  document.getElementById("encryptForm").addEventListener("submit", function(e) {
+  document.getElementById('encryptForm').addEventListener('submit', function(e) {
     e.preventDefault();
 
     // Values
@@ -7,15 +7,21 @@ window.onload = function () {
     var toEncrypt = document.getElementById('encrypt').value;
     var toDecrypt = document.getElementById('decrypt').value;
 
+    if (key.length < 16) {
+      return document.getElementById('error-message-password').classList.remove('hide');
+    } else {
+      document.getElementById('error-message-password').classList.add('hide');
+    }
+
     // Create an encryptor
     var encryptor = require('simple-encryptor')(key);
 
     // To Encrypt
     var encrypted = encryptor.encrypt(toEncrypt);
-    document.getElementById("encrypted").value=encrypted;
+    document.getElementById('encrypted').value=encrypted;
 
     // To Decrypt
     var decrypted = encryptor.decrypt(toDecrypt);
-    document.getElementById("decrypted").value=decrypted;
+    document.getElementById('decrypted').value=decrypted;
   });
 }
